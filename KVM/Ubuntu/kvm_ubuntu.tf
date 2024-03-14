46ff7c3a8a6ef94da8bba6a49d20f228f1e3cfbc
tomkaboris <tomkaboris@gmail.com>
2024-03-14 13:16:00 +0100

    Add comment

--- KVM/Ubuntu/kvm_ubuntu.tf
+++ KVM/Ubuntu/kvm_ubuntu.tf
@@ -34,7 +34,7 @@ resource "libvirt_cloudinit_disk" "commoninit" {
     pool = "default"
 }
 
-# 4. ----> Create the compute vm
+# 5. ----> Create the compute vm
 resource "libvirt_domain" "domain-ubuntu" {
     name = "ubuntu-terraform"
     memory = "512"
