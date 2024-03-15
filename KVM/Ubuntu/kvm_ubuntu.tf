8574076608481cef22f267850e5b6bddfba503cf
Tomka <32951703+tomkaboris@users.noreply.github.com>
2024-03-16 00:33:15 +0100

    Update kvm_ubuntu.tf

--- KVM/Ubuntu/kvm_ubuntu.tf
+++ KVM/Ubuntu/kvm_ubuntu.tf
@@ -7,79 +7,69 @@ terraform {
     }
   }
 }
+
 # instance the provider
 provider "libvirt" {
-    uri = "qemu:///system"
+  uri = "qemu:///system"
 }
 
 # We fetch the latest ubuntu release image from their mirrors
 resource "libvirt_volume" "ubuntu-qcow2" {
-    name = "ubuntu.qcow2"
-    pool = "default" 
-    source = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
-    format = "qcow2"
-}
-
-# Create a network for our VMs
-resource "libvirt_network" "vm_network" {
-    name = "vm_network"
-    addresses = ["10.0.1.0/24"]
-    dhcp {
-	    enabled = true
-    }
-}
-
-# Use CloudInit to add our ssh-key to the instance
-resource "libvirt_cloudinit_disk" "commoninit" {
-    name = "commoninit.iso"
-    pool = "default"
-    user_data = "${data.template_file.user_data.rendered}"
-    network_config = "${data.template_file.network_config.rendered}"
+  name   = "ubuntu.qcow2"
+  pool   = "default"
+  source = "https://cloud-images.ubuntu.com/releases/xenial/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img"
+  format = "qcow2"
 }
 
 data "template_file" "user_data" {
-    template = "${file("${path.module}/cloud_init.cfg")}"
+  template = file("${path.module}/cloud_init.cfg")
 }
 
 data "template_file" "network_config" {
-    template = "${file("${path.module}/network_config.cfg")}"
+  template = file("${path.module}/network_config.cfg")
 }
 
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+  pool           = "default"
+}
 
 # Create the machine
 resource "libvirt_domain" "domain-ubuntu" {
-    name = "ubuntu-terraform"
-    memory = "512"
-    vcpu = 1
+  name   = "ubuntu-terraform"
+  memory = "512"
+  vcpu   = 1
 
-    cloudinit = "${libvirt_cloudinit_disk.commoninit.id}"
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
 
-    network_interface {
-        network_id = "${libvirt_network.vm_network.id}"
-        network_name = "vm_network"
-    }
+  network_interface {
+    network_name = "default"
+  }
 
-  # IMPORTANT
-  # Ubuntu can hang is a isa-serial is not present at boot time.
-  # If you find your CPU 100% and never is available this is why
-    console {
-        type        = "pty"
-        target_port = "0"
-        target_type = "serial"
-    }
+  # IMPORTANT: this is a known bug on cloud images, since they expect a console
+  # we need to pass it
+  # https://bugs.launchpad.net/cloud-images/+bug/1573095
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
 
-    console {
-        type        = "pty"
-        target_type = "virtio"
-        target_port = "1"
-    }
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
 
-    disk {
-        volume_id = "${libvirt_volume.ubuntu-qcow2.id}"
-    }
-    graphics {
-        type = "spice"
-        listen_type = "address"
-        autoport = "true"
-    }
+  disk {
+    volume_id = libvirt_volume.ubuntu-qcow2.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
 }
